499f6233de51c528d1daba2c2feaa5e2d9389f84
Sadia Afrin Anika <141661782+anikasadiaOPT@users.noreply.github.com>
2025-12-18 01:07:30 +0600

    Fix print statements for clarity and output comments

--- 04_day/Challenge_03.py
+++ 04_day/Challenge_03.py
@@ -4,7 +4,7 @@ data = []               # an empty list
 data.append(1)          # .append() -> add new data /value at the end of previous data
 data.append(2)
 data.append(3)
-print(f"After adding all the numbers in a list named data\ndata = {data}")
+print(f"After adding all the numbers in a list named data\ndata = {data}")  # Ouptu: [1, 2, 3]
 
 data.pop()              # remove the element from the last
-print(f"After removing the last number from the list\ndata = {data}")
+print(f"After removing the last number from the list\ndata = {data}")        # Output: [1, 2]

9b3bfac773b1dd9b3ae85c4848b2606b97538e8d
Sadia Afrin Anika <141661782+anikasadiaOPT@users.noreply.github.com>
2025-12-17 19:03:43 +0000

    Implemented the One-Line Architect

---
+++ 04_day/Challenge_04.py
@@ -0,0 +1,9 @@
+# The One-Line Architect
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(f"Before Squaring the even numbers: {numbers}")
+
+numbers = [x**2 for x in numbers if x % 2 == 0]
+
+print(f"After Squaring the even numbers:{numbers}")

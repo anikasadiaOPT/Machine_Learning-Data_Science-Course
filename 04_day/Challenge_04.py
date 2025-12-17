bc157a5f3d3ba793ec183af89faac2a22b147472
Sadia Afrin Anika <141661782+anikasadiaOPT@users.noreply.github.com>
2025-12-18 01:09:37 +0600

    Add output comment for squared even numbers
    Added output comment to the print statement after squaring even numbers.

--- 04_day/Challenge_04.py
+++ 04_day/Challenge_04.py
@@ -6,4 +6,4 @@ print(f"Before Squaring the even numbers: {numbers}")
 
 numbers = [x**2 for x in numbers if x % 2 == 0]
 
-print(f"After Squaring the even numbers:{numbers}")
+print(f"After Squaring the even numbers:{numbers}")    # Output: [4, 16, 36, 64, 100]

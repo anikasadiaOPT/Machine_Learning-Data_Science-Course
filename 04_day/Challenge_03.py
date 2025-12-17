e5211ce386cfe1233ced72f5a202735d1a7b4fe9
Sadia Afrin Anika <141661782+anikasadiaOPT@users.noreply.github.com>
2025-12-17 18:47:01 +0000

    Implemented the Stack Emulator

---
+++ 04_day/Challenge_03.py
@@ -0,0 +1,10 @@
+# The Stack Emulator
+
+data = []               # an empty list
+data.append(1)          # .append() -> add new data /value at the end of previous data
+data.append(2)
+data.append(3)
+print(f"After adding all the numbers in a list named data\ndata = {data}")
+
+data.pop()              # remove the element from the last
+print(f"After removing the last number from the list\ndata = {data}")

dbf3f82048f631c28da68512087a79f45102f33e
Sadia Afrin Anika <141661782+anikasadiaOPT@users.noreply.github.com>
2025-12-20 14:45:37 +0000

    Added the solution of the Pure Return

---
+++ Day_06/Challenge_02.py
@@ -0,0 +1,8 @@
+# The Pure Return 
+
+def add(a,b):
+    print(f"Sum: {a+b}")          # Output:- Sum: 10 
+
+res = add(5,5)
+
+print(res)                        # Output : None ; The function does not return a value

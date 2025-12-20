f5b623ef503c84d1d921ea46a80e822808c03e91
Sadia Afrin Anika <141661782+anikasadiaOPT@users.noreply.github.com>
2025-12-20 15:12:57 +0000

    Added the solution of the Default Gateway

---
+++ Day_06/Challenge_03.py
@@ -0,0 +1,8 @@
+# The Default Gateway
+
+def connect(port=3306):
+    print(f"Connecting to {port}")
+
+
+connect()                   # Output: Connecting to 3306
+connect(5432)               # Output: Connecting to 5432
